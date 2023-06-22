9dc6feeb9de782ec4e80e9845ebf612a6995da14
Shaun K <123934813+paulshaun-kdtk@users.noreply.github.com>
2023-06-22 23:06:05 +0200

    Javascript dynamic popup base

--- index.js
+++ index.js
@@ -1,30 +1,175 @@
-const popupMenu = document.getElementById('MobilePopupMenu');
-const menuTrigger = document.querySelector('.menu-img');
-const menuItems = document.querySelectorAll('.menu_items');
-const closeIcon = document.querySelector('.Xmenu img');
+const openIcon = document.querySelector('.open-icon');
+const closeIcon = document.querySelector('.cancel-icon');
+const mobileMenu = document.querySelector('.mobile-menu');
+const mobileLinks = document.querySelector('.mobile-nav>li');
+const popUp = document.querySelector('.popup-section');
+const projectSection = document.querySelector('.multi-post');
 
-function closePopupMenu() {
-  popupMenu.classList.remove('active');
-}
+const closeMenu = () => {
+  mobileMenu.style.display = 'none';
+  openIcon.style.display = 'block';
+};
 
-menuTrigger.addEventListener('click', function() {
-  popupMenu.classList.toggle('active');
+openIcon.addEventListener('click', () => {
+  openIcon.style.display = 'none';
+  mobileMenu.style.display = 'block';
 });
+closeIcon.addEventListener('click', closeMenu);
+mobileLinks.addEventListener('click', closeMenu);
 
-closeIcon.addEventListener('click', closePopupMenu);
 
-menuItems.forEach(function(item) {
-  item.addEventListener('click', closePopupMenu);
+// Day 2 javascript dynamic js 
+
+const projectDetails = [
+  {
+    name: 'Professional Art Printing Data',
+    description: `A daily selection of privately personalized reads; no accounts or
+                  sign-ups required. Has been the industry's standard.`,
+    details: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+              when an unknown printer took a galley of type and scrambled it 1960s.
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+              when an unknown printer took a galley of type and scrambled it 1960s with the 
+              releax map lapora verita.`,
+    img: './assets/images/project-img.svg',
+    technologies: ['HTML', 'Bootscrap', 'Ruby'],
+    live: '',
+    'github link': 'https://github.com/paulshaun-kdtk/Microverse_Proffessional_Portifolio',
+  },
+  {
+    name: 'Professional Art Printing Data',
+    description: `A daily selection of privately personalized reads; no accounts or
+                  sign-ups required. Has been the industry's standard.`,
+    details: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+              when an unknown printer took a galley of type and scrambled it 1960s.
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+              when an unknown printer took a galley of type and scrambled it 1960s with the 
+              releax map lapora verita.`,
+    img: './assets/images/project-img.svg',
+    technologies: ['HTML', 'Bootscrap', 'Ruby'],
+    live: '',
+    'github link': 'https://github.com/paulshaun-kdtk/Microverse_Proffessional_Portifolio',
+  },
+  {
+    name: 'Professional Art Printing Data',
+    description: `A daily selection of privately personalized reads; no accounts or
+                  sign-ups required. Has been the industry's standard.`,
+    details: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+              when an unknown printer took a galley of type and scrambled it 1960s.
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+              when an unknown printer took a galley of type and scrambled it 1960s with the 
+              releax map lapora verita.`,
+    img: './assets/images/project-img.svg',
+    technologies: ['HTML', 'Bootscrap', 'Ruby'],
+    live: '',
+    'github link': 'https://github.com/paulshaun-kdtk/Microverse_Proffessional_Portifolio',
+  },
+  {
+    name: 'Professional Art Printing Data',
+    description: `A daily selection of privately personalized reads; no accounts or
+                  sign-ups required. Has been the industry's standard.`,
+    details: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+              when an unknown printer took a galley of type and scrambled it 1960s.
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+              when an unknown printer took a galley of type and scrambled it 1960s with the 
+              releax map lapora verita.`,
+    img: './assets/images/project-img.svg',
+    technologies: ['HTML', 'Bootscrap', 'Ruby'],
+    live: '',
+    'github link': 'https://github.com/paulshaun-kdtk/Microverse_Proffessional_Portifolio',
+  },
+  {
+    name: 'Professional Art Printing Data',
+    description: `A daily selection of privately personalized reads; no accounts or
+                  sign-ups required. Has been the industry's standard.`,
+    details: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+              when an unknown printer took a galley of type and scrambled it 1960s.
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+              when an unknown printer took a galley of type and scrambled it 1960s with the 
+              releax map lapora verita.`,
+    img: './assets/images/project-img.svg',
+    technologies: ['HTML', 'Bootscrap', 'Ruby'],
+    live: '',
+    'github link': 'https://github.com/paulshaun-kdtk/Microverse_Proffessional_Portifolio',
+  },
+  {
+    name: 'Professional Art Printing Data',
+    description: `A daily selection of privately personalized reads; no accounts or
+                  sign-ups required. Has been the industry's standard.`,
+    details: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+              when an unknown printer took a galley of type and scrambled it 1960s.
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+              when an unknown printer took a galley of type and scrambled it 1960s with the 
+              releax map lapora verita.`,
+    img: './assets/images/project-img.svg',
+    technologies: ['HTML', 'Bootscrap', 'Ruby'],
+    live: '',
+    'github link': 'https://github.com/paulshaun-kdtk/Microverse_Proffessional_Portifolio',
+  },
+];
+
+const code = document.createElement('div');
+code.classList.add('works-section');
+const allCodes = projectDetails.map((item) => (
+  `<section class="works-item">
+    <article>
+      <h3 class="heading">${item.name}</h3>
+      <p>${item.description}</p>
+      <ul class="works-item-list">
+        <li>HTML</li>
+        <li>Bootscrap</li>
+        <li>Ruby</li>
+      </ul>
+    </article>
+    <button type="button" class="works-btn btn">See project</button>
+  </section>`
+));
+code.innerHTML = allCodes.join('');
+projectSection.appendChild(code);
+
+const worksBtn = document.querySelectorAll('.works-btn, .mp-btn');
+const modal = document.createElement('div');
+modal.className = 'modal';
+worksBtn.forEach((element, elementIndex) => {
+  element.addEventListener('click', () => {
+    popUp.style.display = 'block';
+    modal.innerHTML = `
+    <img class="close-project" src="./assets/images/X.png" alt="">
+    <h3>${projectDetails[elementIndex].name}</h3>
+    <ul>
+      <li>HTML</li>
+      <li>Bootscrap</li>
+      <li>Ruby</li>
+    </ul>
+    <section>
+      <div>
+        <img src=${projectDetails[elementIndex].img} alt="An image of the project">
+      </div>
+      <article>
+        <p>${projectDetails[elementIndex].details}</p>
+        <div>
+          <button class="btn" type="button"><a href='${projectDetails[elementIndex].live}'>See Live</a></button>
+          <button class="btn" type="button"><a href='${projectDetails[elementIndex]['github link']}'>See Source</a></button>
+        </div>
+      </article>
+    </section>
+    `;
+    popUp.appendChild(modal);
+
+    const closeProject = document.querySelector('.close-project');
+    closeProject.addEventListener('click', () => {
+      popUp.style.display = 'none';
+    });
+  });
 });
-
-
-
-function togglePopupMenu() {
-  popupMenu.classList.toggle('active');
-}
-
-// jQuery example
-$('#MobilePopupMenu').click(function() {
-  $(this).toggleClass('active');
-});
-
